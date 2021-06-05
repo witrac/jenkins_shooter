4b26fb79f1a6329913e4812a7e69743f2bb925d5
Jesus Palacios <jesus.palacios@witrac.es>
2021-06-05 23:26:30 +0200

    Publish the result by mqtt subscription

--- jenkins_shooter.py
+++ jenkins_shooter.py
@@ -1,110 +1,75 @@
-# 11b8138de0a1c2695ceeff922eb153d42d
 import time
-import multiprocessing
 import argparse
-# from jenkinsapi.jenkins import Jenkins
-# import jenkinsapi.api
 import jenkins
-# import RPi.GPIO as GPIO
-# GPIO.setmode(GPIO.BCM)
-green_led = 4
-red_led = 17
-# GPIO.setup(green_led, GPIO.OUT)
-# GPIO.setup(red_led, GPIO.OUT)
+import paho.mqtt.client as mqtt
+
 jenkins_username = None
 jenkins_password = None
 
 def get_server_instance():
     jenkins_url = 'url'
-    # server = jenkinsapi.api.get_latest_build(jenkins_url, "master", username=jenkins_username, password=jenkins_password, ssl_verify=False)
     server = jenkins.Jenkins(jenkins_url, username=jenkins_username, password=jenkins_password)
     return server
 
-def get_job_building( server ):
-    """Get job details of each job that is running on the Jenkins instance"""
-    # Refer Example #1 for definition of function 'get_server_instance'
-    # server = get_server_instance()
-    # if server.is_running():
-    #     return "running"
-    # if server.is_good():
-    #     return "good"
+def get_building_job( server ):
+    # Get job details of each job that is running on the Jenkins instance
     result = None
     jobs = server.get_jobs()
     n_branches = len(jobs)
-    print( n_branches )
     for job in jobs:
         color = job["color"]
         if "anime" in color:  
             result = job
             break
+
     return result
-    # return "fail"
-    # for job_name, job_instance in server.get_jobs():
-    #     print ('Job Name:%s' % (job_instance.name))
-    #     print ('Job Description:%s' % (job_instance.get_description()))
-    #     print ('Is Job running:%s' % (job_instance.is_running()))
-    #     print ('Is Job enabled:%s' % (job_instance.is_enabled()))
 
-def get_job_details( server, job ):
-    print( job["name"])
-    next_build = server.next_build_number = server.get_job_info(job["name"])['nextBuildNumber']
-    job_details = server.get_build_info(job["name"], next_build - 1)
-    # print(job_details)
-    # print(job_details["actions"][0]["causes"][0]["userName"])
-    # print(job_details["changeSets"][0]["items"][0]["authorEmail"])
-    print(job_details["building"])
-    print(job_details["result"])
+def get_job_details( server, job, t ):
+    finished_job = False
+    result = None
+    print( "Building " + job["name"] + "..." )
+    while not finished_job:
+        time.sleep( t )
+        next_build = server.next_build_number = server.get_job_info(job["name"])['nextBuildNumber']
+        job_details = server.get_build_info( job["name"], next_build - 1 )
+        # try:
+        #     print( job_details["building"] )
+        #     print( job_details["result"] )
+        #     # print( job_details )
+        #     # print( job_details["actions"][0]["causes"][0]["userName"] )
+        #     # print( job_details["changeSets"][0]["items"][0]["authorEmail"] )
+        # except:
+        #     print( "Email user error" )
 
-def blink():
-    while True:
-        # # GPIO.output(green_led, GPIO.LOW)
-        # # GPIO.output(red_led, GPIO.HIGH)
-        time.sleep(0.5)
-        # # GPIO.output(green_led, GPIO.HIGH)
-        # # GPIO.output(red_led, GPIO.LOW)
-        time.sleep(0.5)
-        print("blink...")
+        if job_details["building"] == False:
+            result = job_details["result"]
+            finished_job = True
 
-def green():
-    print("GREEN")
-    # # GPIO.output(green_led, GPIO.HIGH)
-    # # GPIO.output(red_led, GPIO.LOW)
+    return result
 
-def red():
-    print("RED")
-    # # GPIO.output(green_led, GPIO.LOW)
-    # # GPIO.output(red_led, GPIO.HIGH)
+def publish_result( client, result ):
+    print( "Completed: " + result )
+    try:
+        client.connect( "localhost" )
+        client.publish( "jenkins", result )
+        print( "Published\n" )
+    except:
+        print( "Publication error\n" )
+
 
 def main():
-    # p = multiprocessing.Process(target=blink)
     t = 10
+    client = mqtt.Client( "jenkins" )
     server = get_server_instance()
-    job_building = None
+
     while True:
-        job = get_job_building( server )
+        time.sleep( t )
+        job = get_building_job( server )
         if job:
-            job_building = job
-        if job_building:
-            print("--->",job)
-            get_job_details( server, job )
-        # print(result)
-        # if result == "running" and p.is_alive() == False:
-        #     p = multiprocessing.Process(target=blink)
-        #     p.start()
-        #     t = 10
-        # elif result == "good":
-        #     if p.is_alive():
-        #         p.terminate()
-        #     green()
-        #     t = 30
-        # elif result == "fail":
-        #     if p.is_alive():
-        #         p.terminate()
-        #     red()
-        #     t = 30
-        # else:
-        #     print("Compilando...")
-        time.sleep(t)
+            result = get_job_details( server, job, t )
+            publish_result( client, result )
+            job = None
+
     return 1
 
 def parse_args():
